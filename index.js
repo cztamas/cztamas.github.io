9a38dbfcbe26c7e5416a618aad51e72971330d08
cztamas <tamas.czank@gmail.com>
2018-02-21 15:42:40 +0100

    data

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const button1 = document.getElementById("button1");
 const button2 = document.getElementById("button2");
 
+const data1 = `
+	<html>
+		<body>
+			<h1>This is data1</h1>
+		</body>
+	</html>
+`;
+
+const data2 = `
+	<html>
+		<body>
+			<h1>This is data2</h1>
+		</body>
+	</html>
+`;
+
 button1.addEventListener("click", () => {
-	window.parent.postMessage("data1", "*");
+	window.parent.postMessage(data1, "*");
 });
 
 button2.addEventListener("click", () => {
-	window.parent.postMessage("data2", "*");
+	window.parent.postMessage(data2, "*");
 });
