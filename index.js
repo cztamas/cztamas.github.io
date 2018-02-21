79c9b0ef6c913a4424f614e6c89c3816eaf7f483
cztamas <tamas.czank@gmail.com>
2018-02-21 16:16:52 +0100

    images

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const button2 = document.getElementById("button2");
 const data1 = `
 	<html>
 		<body>
-			<h1>This is data1</h1>
+			<h1>This is an Arnold</h1>
+			<img src="https://chamaileon.io/img/team/arnold.png" />
 		</body>
 	</html>
 `;
@@ -12,7 +13,8 @@ const data1 = `
 const data2 = `
 	<html>
 		<body>
-			<h1>This is data2</h1>
+			<h1>This is not an Arnold</h1>
+			<img src="http://impressivemagazine.com/wp-content/uploads/2013/10/beer_glasses.jpeg" />
 		</body>
 	</html>
 `;
